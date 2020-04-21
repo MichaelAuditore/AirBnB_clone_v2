af2fe49b5e6cb671b366e2f6e53058423c73ffea
MichaelAuditore <michael_v613@hotmail.com>
2020-04-21 18:32:06 -0500

    [Fix] Order of variables and content of list

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,35 +8,35 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/states')
-def states():
-        """Displays a body response with a
-        list of all states sorted from A->Z"""
+def only_states():
+        """ send all states
+        """
         states = storage.all(State)
-        states = [[states[s].name, states[s].id] for s in states]
-        states.sort()
-        return render_template('9-states.html', S=states, F=0)
+        states = [[states[s].id, states[s].name] for s in states]
+        states = sorted(states, key=lambda x: x[1])
+        return (render_template('9-states.html', S=states, C=None, F=3))
 
 
 @app.route('/states/<id>')
-def list_states(id):
-        """Displays a body response with a
-        list of all states sorted from A->Z"""
-        states = storage.all(State)
-        cities = storage.all(City)
-        flag = 0
+def city_of_state_id(id):
+        """ cities of state id
+        """
         new = []
+        flag = 0
+        states = storage.all(State)
         for s in states:
                 if (id == states[s].id):
                         flag = 1
-                        new.append([states[s].id, states[s].name])
+                        new.append((states[s].id, states[s].name))
         if (flag == 0):
                 return (render_template('9-states.html', S=None, C=None, F=0))
-        city = []
-        for c in cities:
-                if (cities[c].state_id == new[0][0]):
-                        city.append([cities[c].name, cities[c].id])
-        city.sort()
-        return (render_template('9-states.html', S=new, C=city, F=1))
+        states = storage.all(City)
+        cities = []
+        for city in states:
+                if (states[city].state_id == new[0][0]):
+                        cities.append((states[city].id, states[city].name))
+        cities = sorted(cities, key=lambda x: x[1])
+        return (render_template('9-states.html', S=new, C=cities, F=1))
 
 
 @app.teardown_appcontext

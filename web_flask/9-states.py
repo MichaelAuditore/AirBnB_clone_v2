9ffad4039a4a04294c0aefb810fe5df3288ae008
MichaelAuditore <michael_v613@hotmail.com>
2020-04-21 18:10:26 -0500

    [Fix] Another way to get all states and cities by states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,22 +8,36 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/states')
+def states():
+        """Displays a body response with a
+        list of all states sorted from A->Z"""
+        states = storage.all(State)
+        states = [[states[s].name, states[s].id] for s in states]
+        states.sort()
+        return render_template('9-states.html', S=states, F=0)
+
+
 @app.route('/states/<id>')
-def list_states(id=None):
+def list_states(id):
         """Displays a body response with a
         list of all states sorted from A->Z"""
         states = storage.all(State)
         cities = storage.all(City)
-        flag = 0;
-        if id is not None:
-                flag = 1;
-        states = [[states[s].name, states[s].id] for s in states]
-        cities = [[cities[c].name, cities[c].state_id, cities[c].id]
-                  for c in cities]
-        states.sort()
-        cities.sort()
-        return (render_template('9-states.html', S=states,
-                                C=cities, F=flag, ID=id))
+        flag = 0
+        new = []
+        for s in states:
+                if (id == states[s].id):
+                        flag = 1
+                        new.append([states[s].id, states[s].name])
+        if (flag == 0):
+                return (render_template('9-states.html', S=None, C=None, F=0))
+        city = []
+        for c in cities:
+                if (cities[c].state_id == new[0][0]):
+                        city.append([cities[c].id, cities[c].name])
+        city.sort()
+        return (render_template('9-states.html', S=new, C=city, F=1))
+
 
 @app.teardown_appcontext
 def close(self=None):

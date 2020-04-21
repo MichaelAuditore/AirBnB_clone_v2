18a79d1c4d063fe5ce201a500d99179421e0d20f
MichaelAuditore <michael_v613@hotmail.com>
2020-04-21 16:57:48 -0500

    [Fix] Another Way to print all states or cities by state id

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,113 +7,23 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-        """Return a body response containing a string Hello HBNB!"""
-        return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hbnb():
-        """Return a body response containing a string HBNB"""
-        return 'HBNB'
-
-
-@app.route('/c/<string>')
-def c(string):
-        """Return a body response containing
-        a string with the value of variable passed to the URL"""
-        return 'C %s' % escape(string.replace('_', ' '))
-
-
-@app.route('/python', defaults={'string': 'is_cool'})
-@app.route('/python/<string>')
-def python(string='is_cool'):
-        """Return a body response containing a string with the value
-        of variable passed to the URL"""
-        return 'Python %s' % escape(string.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def number(n):
-        """Displays a body response only if type of n is integer"""
-        return '%i is a number' % n
-
-
-@app.route('/number_template/<int:n>')
-def number_template(n):
-        """Display a number template"""
-        return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def even_or_odd(n):
-        """Display a number template"""
-        return render_template('6-number_odd_or_even.html', n=n)
-
-
-@app.route('/states_list')
-def states():
-        """Displays a body response with a
-        list of all states sorted from A->Z"""
-        states = storage.all(State)
-        new_states = []
-        for state in states:
-                new_states.append([states[state].name, states[state].id])
-        new_states.sort()
-        return render_template('7-states_list.html', data=new_states)
-
-
-@app.route('/cities_by_states')
-def cities_by_states():
-        """Displays a body response with a
-        list of all states sorted from A->Z"""
-        states = storage.all(State)
-        new_cities = []
-        new_states = []
-
-        for state in states:
-                new_states.append([states[state].id, states[state].name])
-                cities = states[state].cities
-                for city in cities:
-                        new_cities.append([city.name, states[state].name,
-                                           city.id])
-        new_states.sort()
-        new_cities.sort()
-        return (render_template('8-cities_by_states.html', S=new_states,
-                                C=new_cities))
-
-
 @app.route('/states')
 @app.route('/states/<id>')
 def list_states(id=None):
         """Displays a body response with a
         list of all states sorted from A->Z"""
         states = storage.all(State)
-        new_cities = []
-        new_states = []
-
-        if (id is not None):
-                for state in states:
-                        if (id == states[state].id):
-                                new_states.append([states[state].id,
-                                                   states[state].name])
-                                cities = states[state].cities
-                                for city in cities:
-                                        new_cities.append([city.name,
-                                                           states[state].name,
-                                                           city.id])
-                new_cities.sort()
-        else:
-                for state in states:
-                        new_states.append([states[state].id,
-                                          states[state].name])
-
-        new_states.sort()
-        flag = 1 if id is not None else 0
-        return (render_template('9-states.html', S=new_states,
-                                C=new_cities, F=flag))
-
+        cities = storage.all(City)
+        flag = 0;
+        if id is not None:
+                flag = 1;
+        states = [[states[s].name, states[s].id] for s in states]
+        cities = [[cities[c].name, cities[c].state_id, cities[c].id]
+                  for c in cities]
+        states.sort()
+        cities.sort()
+        return (render_template('9-states.html', S=states,
+                                C=cities, F=flag, ID=id))
 
 @app.teardown_appcontext
 def close(self=None):
